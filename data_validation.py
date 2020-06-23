e003eddbfba4dc16c9b9e5565d098cb3f94cb0b1
Chris Potter <christhepcguy@gmail.com>
2020-06-23 19:30:15 -0400

    Updated to look at file import sample. Improved error handling.

--- data_validation.py
+++ data_validation.py
@@ -1,32 +1,59 @@
+from durable.engine import MessageNotHandledException, MessageObservedException
 from fastapi import FastAPI
 from pydantic import BaseModel
 from durable.lang import *
 import json
+import random
+from datetime import date, datetime
 
 app = FastAPI()
 
 
-class Fact(BaseModel):
-    subject: str
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class DataSource(BaseModel):
+    id: int
+    data_source_id: int
+    name: str
+    row_count: int
+    effective_date: date
+    expiration_date: date
 
 
 @app.post("/evaluate/{ruleset}")
-async def evaluate(ruleset: str, fact: Fact):
+async def evaluate(ruleset: str, fact: DataSource):
     # Cast from Fact -> JSON -> Dict
     f = json.loads(fact.json())
-    result = assert_fact(ruleset, f)
+    try:
+        # Using post() instead of assert_fact() avoids throwing an error when similar data in the object matches
+        result = post(ruleset, f)
+    except MessageNotHandledException as ex:
+        print("Event matched no rules: {0}".format(ex.message))
+        result = {"result": "Data passed validation ruleset '{0}'".format(ruleset)}
+        print(result)
+    except MessageObservedException as ex:
+        print("Message has already been observed: {0}".format(ex.message))
+        result = {"result": "Data may have passed validation, but validation states that fact was already evaluated."}
     return result
 
 
-with ruleset('missing_data'):
-    @when_all(m.subject == '')
-    def missing_subject(c):
-        result = {"result": "Subject is empty. A value is required."}
+with ruleset('data_sources'):
+    @when_all((m.data_source_id == 1) & (m.row_count < 100))  # Sample Data Source... each would be mapped independently
+    def warn_below_minimum_record_count(c):
+        result = {"result": "WARNING: {0} - Minimum row_count is 100. Received {1}...".format(c.m.name, c.m.row_count)}
         print(result)
         return result
 
+
+    @when_any((m.row_count > 0) & (m.row_count < 50))
+    def error_below_minimum_record_count(c):
+        result = {
+            "result": "ERROR: {0} - Minimum row count is below hard limit of 50 records! Received {1}".format(c.m.name,
+                                                                                                              c.m.row_count)}
+        print(result)
+        return result
+
+with ruleset('data_missing'):
+    @when_all(m.data_source_id == 1)
+    def warn_empty_row_count(c):
+        result = {"result": "ERROR: {0} - row_count is a required field. Received {1}".format(c.m.name, c.m.row_count)}
+        print(result)
+        return result
